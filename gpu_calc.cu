28f2c5cbb0245ffec4201d4e1b3689fb95db1983
gogotanaka <mail@tanakakazuki.com>
2016-07-17 23:00:19 +0900

    Optimize reduction

--- gpu_calc.cu
+++ gpu_calc.cu
@@ -8,11 +8,17 @@ const int BLOCK_SIZE = 128;
 
 __global__ void device_dft_idft(double *d_re, double *d_im, double *d_temp_re, double *d_temp_im, int flag, int num)
 {
-  int i = blockDim.x * blockIdx.x + threadIdx.x;
+  int i = BLOCK_SIZE * blockIdx.x + threadIdx.x;
   int j;
+  double tmp_c, tmp_s;
+  double tmp_2_pi_i = (2 * PI * i) / num;
+
   for(j=0; j<num; j++){
-    d_temp_re[i] += d_re[j]*cos(2*PI*i*j/num) + flag*d_im[j]*sin(2*PI*i*j/num);
-    d_temp_im[i] += -flag*d_re[j]*sin(2*PI*i*j/num) + d_im[j]*cos(2*PI*i*j/num);
+    tmp_c = cos(tmp_2_pi_i*j);
+    tmp_s = sin(tmp_2_pi_i*j);
+
+    d_temp_re[i] += d_re[j]*tmp_c + flag*d_im[j]*tmp_s;
+    d_temp_im[i] += -flag*d_re[j]*tmp_s + d_im[j]*tmp_c;
   }
   if(flag == IDFT){
     d_temp_re[i] /= num;
@@ -33,9 +39,6 @@ void launch_kernel(double *h_re, double *h_im, int num, int flag)
   cudaMalloc((void **)&d_re, doubleNumSize);
   cudaMalloc((void **)&d_im, doubleNumSize);
 
-  cudaHostAlloc((void **)&h_temp_re, doubleNumSize, cudaHostAllocDefault);
-  cudaHostAlloc((void **)&h_temp_im, doubleNumSize, cudaHostAllocDefault);
-
   if((h_temp_re = (double*)malloc(doubleNumSize)) == NULL){
     fprintf(stderr, "Allocationerror!\n");
   }
@@ -55,6 +58,7 @@ void launch_kernel(double *h_re, double *h_im, int num, int flag)
   cudaMemcpy(d_im, h_im, doubleNumSize, cudaMemcpyHostToDevice);
 
   device_dft_idft<<<dim_grid, dim_block>>>(d_re, d_im, d_temp_re, d_temp_im, flag, num);
+
   cudaMemcpy(h_re, d_temp_re, doubleNumSize, cudaMemcpyDeviceToHost);
   cudaMemcpy(h_im, d_temp_im, doubleNumSize, cudaMemcpyDeviceToHost);
 
