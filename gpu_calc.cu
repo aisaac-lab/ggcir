a8f687994a201e4536ceed2d4e16c54378306ccc
gogotanaka <mail@tanakakazuki.com>
2016-07-17 19:02:41 +0900

    Implement gpu_calc.cu

--- gpu_calc.cu
+++ gpu_calc.cu
@@ -3,55 +3,87 @@
 #include <math.h>
 #include "calculation.h"
 
-__global__ void device_dft_idft()
+const int BLOCK_SIZE = 256;
+
+__global__ void device_dft_idft(double *d_re, double *d_im, double *d_temp_re, double *d_temp_im, int flag, int num)
 {
-  //This kernel calculates the dft and idft on GPU.
-  //Please modify this kernel!!
+  int i = blockDim.x * blockIdx.x + threadIdx.x;
+
+  int j;
+  for(j=0; j<num; j++){
+    d_temp_re[i] += d_re[j]*cos(2*PI*i*j/num) + flag*d_im[j]*sin(2*PI*i*j/num);
+    d_temp_im[i] += -flag*d_re[j]*sin(2*PI*i*j/num) + d_im[j]*cos(2*PI*i*j/num);
+
+    // d_temp_re[i] += (float)i;
+    // d_temp_im[i] += (float)i;
+  }
+  if(flag == IDFT){
+    d_temp_re[i] /= num;
+    d_temp_im[i] /= num;
+  }
 }
 
-void launch_kernel(double *re, double *im, int num, int flag)
+void launch_kernel(double *h_re, double *h_im, int num, int flag)
 {
   //This function launches the dft kernel.
   //Please modify this function for dft kernel .
   //You need to allocate the device memory and so on in this function.
 
   //device_dft_idft<<<blockDim, threadDim>>>(argment1, argment2, etc...);
-  int i, j;
-  double *temp_re, *temp_im;
+  int i;
+  double *d_re, *d_im;
+  double *d_temp_re, *d_temp_im;
+  double *h_temp_re, *h_temp_im;
+  dim3 dim_grid(num/BLOCK_SIZE, 1, 1), dim_block(num, 1, 1);
 
-  if((temp_re = (double*)malloc(sizeof(double)*num)) == NULL){
+  cudaMalloc((void **)&d_temp_re, sizeof(double) * num);
+  cudaMalloc((void **)&d_temp_im, sizeof(double) * num);
+  cudaMalloc((void **)&d_re, sizeof(double) * num);
+  cudaMalloc((void **)&d_im, sizeof(double) * num);
+
+  if((h_temp_re = (double*)malloc(sizeof(double)*num)) == NULL){
     fprintf(stderr, "Allocationerror!\n");
-    return 1;
   }
 
-  if((temp_im = (double*)malloc(sizeof(double)*num)) == NULL){
+  if((h_temp_im = (double*)malloc(sizeof(double)*num)) == NULL){
     fprintf(stderr, "Allocationerror!\n");
-    free(temp_re);
-    return 1;
+    free(h_temp_re);
   }
 
   for(i=0; i<num; i++){
-    temp_re[i] = temp_im[i] = 0.0;
+    h_temp_re[i] = h_temp_im[i] = 0.0;
   }
 
+  cudaMemcpy(d_temp_re, h_temp_re, sizeof(double) * num, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_temp_im, h_temp_im, sizeof(double) * num, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_re, h_re, sizeof(double) * num, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_im, h_im, sizeof(double) * num, cudaMemcpyHostToDevice);
+
+  // device_dft_idft<<<dim_grid, dim_block>>>(d_re, d_im, d_temp_re, d_temp_im, flag, num);
+  // cudaMemcpy(h_re, d_temp_re, sizeof(double) * num, cudaMemcpyDeviceToHost);
+  // cudaMemcpy(h_im, d_temp_im, sizeof(double) * num, cudaMemcpyDeviceToHost);
+
   for(i=0; i<num; i++){
-    for(j=0; j<num; j++){
-      temp_re[i] += re[j]*cos(2*PI*i*j/num) + flag*im[j]*sin(2*PI*i*j/num);
-      temp_im[i] += -flag*re[j]*sin(2*PI*i*j/num) + im[j]*cos(2*PI*i*j/num);
+    for(int j=0; j<num; j++){
+      h_temp_re[i] += h_re[j]*cos(2*PI*i*j/num) + flag*h_im[j]*sin(2*PI*i*j/num);
+      h_temp_im[i] += -flag*h_re[j]*sin(2*PI*i*j/num) + h_im[j]*cos(2*PI*i*j/num);
     }
-  if(flag == IDFT){
-    temp_re[i] /= num;
-    temp_im[i] /= num;
-  }
+    if(flag == IDFT){
+      h_temp_re[i] /= num;
+      h_temp_im[i] /= num;
+    }
   }
 
   for(i=0; i<num; i++){
-    re[i] = temp_re[i];
-    im[i] = temp_im[i];
+    h_re[i] = h_temp_re[i];
+    h_im[i] = h_temp_im[i];
   }
 
-  free(temp_re);
-  free(temp_im);
+  cudaFree(d_re);
+  cudaFree(d_im);
+  cudaFree(d_temp_re);
+  cudaFree(d_temp_im);
 
-  return 0;
+  free(h_temp_re);
+  free(h_temp_im);
 }
